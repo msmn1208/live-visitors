e89ec35dbdd3c6b39757795f8d23d52fce8bcad4
msmn1208 <100920644+msmn1208@users.noreply.github.com>
2025-07-22 12:23:49 +0200

    Delete api/visitors.js

--- api/visitors.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const min = 120;
-  const max = 420;
-
-  const visitors = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  res.setHeader('Cache-Control', 'no-store');
-  res.status(200).json({ visitors });
-}
